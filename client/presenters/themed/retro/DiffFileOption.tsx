a87a5afb3552c6cc431a92d11a8aedd7eb5d08cf
Alexis Haim <alexishaim@outlook.fr>
2025-09-25 08:25:17 +0700

    With file status badge on retro diff file option

--- client/presenters/themed/retro/DiffFileOption.tsx
+++ client/presenters/themed/retro/DiffFileOption.tsx
@@ -1,5 +1,6 @@
 import RetroCheckbox from "./Checkbox";
 import { ThemedFileOptionProps } from "../../headless/ModifiedFilesList";
+import { DiffFileStatus } from "../../../domain/diff";
 
 export default function RetroDiffFileOption({
   file,
@@ -8,6 +9,13 @@ export default function RetroDiffFileOption({
   onContextMenu,
   toggleStaging,
 }: ThemedFileOptionProps) {
+  const colors: { [K in DiffFileStatus]: string[] } = {
+    ADDED: ["border-green-500", "bg-green-500"],
+    MODIFIED: ["border-yellow-400", "bg-yellow-400"],
+    MOVED: ["border-blue-600", "bg-blue-600"],
+    REMOVED: ["border-rose-700", "bg-rose-700"],
+  };
+
   return (
     <div
       className={`font-retro flex cursor-pointer items-center py-[4px] pl-2 ${isSelected ? "bg-retro-active text-white" : "hover:bg-retro-pressed bg-white text-black"}`}
@@ -20,6 +28,15 @@ export default function RetroDiffFileOption({
         onClick={toggleStaging}
         className="mr-2"
       />
+
+      <span
+        className={`h-[20px] w-[20px] border-[2px] ${colors[file.status][0]} flex items-center justify-center mr-3 ml-1`}
+      >
+        <span
+          className={`h-[8px] w-[8px] rounded-full ${colors[file.status][1]}`}
+        ></span>
+      </span>
+
       {file.displayPaths.join("->")}
     </div>
   );
